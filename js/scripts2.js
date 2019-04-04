2d92deef07a2b6b19b4a52820eedab63a9439c52
brooke kullberg and hannah melendy <brooke@>
2019-04-04 15:52:04 -0700

    add function around business logic; move variable to be output into the output function

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,28 +1,26 @@
 //Business Logic
-userInput = 89;
 var arabicNum = [1,4,5,9,10,40,50,90,100,400,500,900,1000];
 var romanNum = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"];
-var romanNumOutput = "";
 
-for (var i = arabicNum.length-1; i >= 0; i--) {
-  while (userInput >= arabicNum[i]) {
+var output = function(userInput) {
+  var romanNumOutput = "";
+  for (var i = arabicNum.length-1; i >= 0; i--) {
+    while (userInput >= arabicNum[i]) {
 
-    romanNumOutput += romanNum[i];
+      romanNumOutput += romanNum[i];
 
-    userInput -= arabicNum[i];
+      userInput -= arabicNum[i];
+    }
   }
+  return romanNumOutput;
 }
-
-console.log(romanNumOutput);
 
 // User Interface Logic
 $(document).ready(function(){
   $("#numSwitchForm").submit(function(event){
     event.preventDefault();
     var userInput = parseInt($("#userInput").val());
-    console.log(userInput);
-    var result = isNumOnethruThree(userInput);
-    console.log(result);
+    var result = output(userInput);
     $("#romanNumeral").text(result);
   });
 });
