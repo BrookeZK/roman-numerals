089dad9c55cf15ffa05c9a711e337460d56bc7ef
brooke kullberg and hannah melendy <brooke@>
2019-04-04 14:59:17 -0700

    add correctly functional logic to business logic in js

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,15 +1,19 @@
 //Business Logic
-
+userInput = 89;
 var arabicNum = [1,4,5,9,10,40,50,90,100,400,500,900,1000];
 var romanNum = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"];
 var romanNumOutput = "";
 
 for (var i = arabicNum.length-1; i >= 0; i--) {
-  while (userInput >= arabicNum) {
-    romanNumOutput = userInput.location[i] replaced with arabicNum.location[i];
-    userInput - arabicNum
+  while (userInput >= arabicNum[i]) {
+
+    romanNumOutput += romanNum[i];
+
+    userInput -= arabicNum[i];
   }
 }
+
+console.log(romanNumOutput);
 
 // User Interface Logic
 $(document).ready(function(){
